82a338cebdc860fd9d65adc447629656edb54f6e
Clement1 David <clement1.david@epita.fr>
2020-10-07 21:12:54 +0200

    2:Counter repetition

--- src/Game/PlayerIA/TalusPlayer.php
+++ src/Game/PlayerIA/TalusPlayer.php
@@ -14,10 +14,34 @@ class TalusPlayer extends Player
     protected $mySide;
     protected $opponentSide;
     protected $result;
+    
+    public function Onechoice($lastoppo)
+    {
+        $count = 0;
+        $oppo_array =  $this->result->getChoicesFor($this->opponentSide);
+        for ($k = 1; $k < count($oppo_array); $k++)
+        {
+            if ($oppo_array[$k] == $oppo_array[$k - 1])
+                $count++;
+        }
+        if ($count ==  $this->result->getNbRound())
+        {
+            if ($lastoppo == 'scissors')
+                return parent::rockChoice();
+            elseif ($lastoppo == 'rock')
+                return parent::paperChoice();
+            return parent::scissorsChoice();
+
+        }
+        return;
+        
+    }
 
     public function getChoice()
     {
-        $lastoppo=$this->result->getLastChoiceFor($this->opponentSide);
+        $mylast =  $this->result->getLastChoiceFor($this->mySide);
+        $lastoppo = $this->result->getLastChoiceFor($this->opponentSide);
+        echo $this->Onechoice($lastoppo);
         if ($lastoppo == 'scissors')
             return parent::rockChoice();
         elseif ($lastoppo == 'rock')
